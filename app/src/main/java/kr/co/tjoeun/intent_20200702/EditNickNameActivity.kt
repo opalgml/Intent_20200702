b2610a8cbdeb42e8d1804276d649f8e84a183ef8
김미희 <opalgml@naver.com>
2020-07-02 21:38:45 +0900

    첫번째 액티비티 추가 실습 -6. 입력값 리턴

--- app/src/main/java/kr/co/tjoeun/intent_20200702/EditNickNameActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20200702/EditNickNameActivity.kt
@@ -1,12 +1,31 @@
 package kr.co.tjoeun.intent_20200702
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nick_name.*
 
 class EditNickNameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nick_name)
+
+        okBtn.setOnClickListener {
+
+            val inputNick = nickNameEdt.text.toString()
+
+//            finish()  데이터 리턴
+            val resultIntent = Intent()
+//            변수에 리턴 값 입력
+            resultIntent.putExtra("nickName", inputNick)
+//            "입력완료" 버튼으로 처리함을 명시 + 결과용 Intent 첨부
+            setResult(Activity.RESULT_OK)
+
+//            처리 완료 시 종료
+            finish()
+
+        }
     }
 }
